4e7cad934ffdfea4eaae23de8f7c1d689a9b1a84
重装小秃 <103594311+OvertimeBunny@users.noreply.github.com>
2024-06-05 14:14:36 +0800

    Update Install-Windows.sh

--- Install-Windows.sh
+++ Install-Windows.sh
@@ -1,199 +1,167 @@
 #!/bin/bash
 
-# Rabbit TRSS Yunzai 安装脚本
-NAME=v1.0.0;VERSION=202306010
-R="\033[1;31m" G="\033[1;32m" Y="\033[1;33m" C="\033[1;36m" B="[1;m" O="[m"
-
-abort() { echo -e "$R! $@$O"; exit 1; }
-
-export LANG=zh_CN.UTF-8
-
-DIR="${DIR:-$HOME/Yunzai}"
-CMD="${CMD:-rabbit}"
-CMDPATH="${CMDPATH:-/usr/local/bin}"
+# Rabbit-TRSS-Yunzai 安装脚本 作者：重装小兔 🐰
+NAME=v1.0.0; VERSION=202306010
+R="\033[1;31m" G="\033[1;32m" Y="\033[1;33m" C="\033[1;36m" B="\033[m" O="\033[m"
 
 echo -e "$B————————————————————————————
 $R Rabbit$Y TRSS$G Yunzai$C Install$O Script
      $G$NAME$C ($VERSION)$O
 $B————————————————————————————
-      $G作者：$C重装小兔 🐰$O
-"
+      $G作者：$C重装小兔 🐰$O"
 
-echo -e "$G 欢迎使用 Rabbit-TRSS-Yunzai ! 作者：重装小兔 🐰$O"
+abort() {
+  echo -e "$R! $@$O"
+  exit 1
+}
 
-abort_update() { echo -e "$R! $@$O"; [ "$N" -lt 10 ] && { ((N++)); download; } || abort "你他喵的网络是怎么回事！给我好好检查你的网络环境！"; }
+export LANG=zh_CN.UTF-8
+[ "$(uname)" = Linux ] || export MSYS=winsymlinks
 
-install_dependencies() {
-  if [ "$(uname)" == "Linux" ]; then
-    if ! type pacman &>/dev/null; then
-      abort "找不到 pacman 命令，请确认安装了正确的 Arch Linux 环境"
-    fi
-    echo -e "$Y- 正在设置清华大学源$O"
-    sudo bash -c 'echo -e "[archlinuxcn]\nServer = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\$arch" > /etc/pacman.d/mirrorlist'
-    sudo pacman -Syy
-    sudo pacman-key --init
-    sudo pacman-key --populate archlinux
-    sudo pacman -Syy archlinux-keyring
-    sudo pacman -Syu --noconfirm --needed --overwrite "*" curl git nodejs npm || abort "依赖安装失败"
-    sudo npm install -g pnpm || abort "pnpm 安装失败"
-  elif [ "$(uname -o)" == "Msys" ]; then
-    echo -e "$Y- 正在安装依赖$O"
-    pacman -Syy --noconfirm --needed --overwrite "*" curl git nodejs npm || abort "依赖安装失败"
-    npm install -g pnpm || abort "pnpm 安装失败"
-  else
-    abort "不支持的系统环境"
-  fi
+DIR="${DIR:-$HOME/Yunzai}"
+CMD="${CMD:-rabbit}"
+CMDPATH="${CMDPATH:-/usr/local/bin}"
+
+type pacman &>/dev/null || abort "找不到 pacman 命令，请确认安装了正确的 Arch Linux 环境"
+type curl dialog &>/dev/null || {
+  echo -e "$Y- 正在安装依赖$O"
+  pacman -Syu --noconfirm --needed --overwrite "*" curl dialog git nodejs npm || abort "依赖安装失败"
+}
+
+abort_update() {
+  echo -e "$R! $@$O"
+  [ "$N" -lt 10 ] && {
+    ((N++))
+    download
+  } || abort "脚本下载失败，请检查网络，并尝试重新下载"
 }
 
 download() {
   case "$N" in
-    1) Server="Gitee" URL="https://gitee.com/TimeRainStarSky/Yunzai";;
-    2) Server="GitHub" URL="https://github.com/TimeRainStarSky/Yunzai";;
+  1)
+    Server="Gitee"
+    URL="https://gitee.com/TimeRainStarSky/Yunzai"
+    ;;
+  2)
+    Server="GitHub"
+    URL="https://github.com/TimeRainStarSky/Yunzai"
+    ;;
   esac
-
-  echo -e "$Y- 正在从 $Server 服务器 下载文件$O"
-  if [ -d "$DIR" ]; then
-    echo -e "$Y- 删除已有目录：$DIR$O"
-    rm -rf "$DIR"
-  fi
-  mkdir -vp "$DIR" && git clone --depth 1 "$URL" "$DIR" || abort_update "下载失败"
-  mkdir -vp "$CMDPATH" && echo -n "cd '$DIR' && node app.js"' "$@"' > "$CMDPATH/$CMD" && chmod 755 "$CMDPATH/$CMD" || abort "脚本执行命令 $CMDPATH/$CMD 设置失败，手动执行命令：cd '$DIR' && node app.js"
-  if [ -n "$MSYS" ]; then
-    type powershell &>/dev/null && powershell -c '$ShortCut=(New-Object -ComObject WScript.Shell).CreateShortcut([System.Environment]::GetFolderPath("Desktop")+"\'"$(basename "$DIR"|tr '_' ' ')"'.lnk")
-$ShortCut.TargetPath="'"$(cygpath -w /msys2.exe)"'"
-$ShortCut.Arguments="'"$CMD"'"
-$ShortCut.Save()'
-  else
-    type wsl.exe powershell.exe &>/dev/null && powershell.exe -c '$ShortCut=(New-Object -ComObject WScript.Shell).CreateShortcut([System.Environment]::GetFolderPath("Desktop")+"\'"$(basename "$DIR"|tr '_' ' ')"'.lnk")
-$ShortCut.TargetPath="'"$(command -v wsl.exe|sed -E 's|/mnt/([a-z]*)/|\1:\\|;s|/|\\|g')"'"
-$ShortCut.Arguments="'"$CMD"'"
-$ShortCut.Save()'
-  fi
-  echo -e "$G- 脚本安装完成，启动命令：$C$CMD$O"
-  exit
+  echo -e "\n  正在从 $Server 服务器 下载文件"
+  git clone --depth 1 "$URL" "$DIR" || abort_update "下载失败"
 }
 
-install_plugins() {
-  cd "$DIR/plugins"
-  echo -e "$Y- 安装常用插件$O"
-  git clone --depth 1 https://gitee.com/OvertimeBunny/trss-plugin.git || abort "TRSS-Plugin 安装失败"
-  git clone --depth 1 https://gitee.com/yoimiya-kokomi/miao-plugin.git || abort "Miao-Plugin 安装失败"
-  git clone --depth 1 https://gitee.com/guoba-yunzai/guoba-plugin.git || abort "Guoba-Plugin 安装失败"
-  cd "$DIR"
-}
+echo -e "\n$Y- 正在下载脚本$O"
+N=1
+download
 
-start_test() {
-  echo -e "$Y- 正在启动测试$O"
-  node app.js &
-  sleep 5
-  echo -e "$Y- 加载配置文件$O"
-  if [ -d "$DIR/data" ]; then
-    echo -e "$Y- 监听文件位置：Yunzai/data$O"
-  else
-    echo -e "$R! Yunzai/data 文件加载失败$O"
-    exit 1
-  fi
-  kill %1
-  main_menu
-}
+echo -e "$Y- 安装项目依赖$O"
+cd "$DIR" || abort "目录不存在：$DIR"
+npm install || abort "项目依赖安装失败"
 
-main_menu() {
+echo -e "$Y- 安装插件$O"
+cd plugins
+git clone --depth 1 https://gitee.com/OvertimeBunny/trss-plugin.git TRSS-Plugin
+cd TRSS-Plugin && npm install && cd ..
+
+git clone --depth 1 https://gitee.com/yoimiya-kokomi/miao-plugin.git miao-plugin
+cd miao-plugin && npm install && cd ..
+
+git clone --depth 1 https://gitee.com/guoba-yunzai/guoba-plugin.git guoba-plugin
+cd guoba-plugin && npm install && cd ..
+
+echo -e "$G- 插件安装完成，即将启动Yunzai$O"
+node app &
+sleep 5
+
+echo -e "$Y- 加载配置文件$O"
+if [ -d "$DIR/data" ]; then
+  echo -e "$Y- 监听文件位置：$DIR/data$O"
+else
+  echo -e "$R! 配置文件加载失败$O"
+  exit 1
+fi
+
+kill %1
+
+trap 'Main_Menu' SIGINT
+
+Main_Menu() {
   clear
   echo -e "$Y- 回来了小老弟？给你检查一下依赖$O"
-  cd "$DIR"
-  pnpm update
-  pnpm install
+  cd "$DIR" || abort "目录不存在：$DIR"
+  npm update
+  npm install
 
-  echo '请选择你需要的适配器：'
-  echo '1：QQBot（官方机器人）'
-  echo '2：ICQQ（普通机器人）'
-  echo '3：NTQQ'
-  read -p '选择一个选项: ' adapter_choice
+  echo "请选择你需要的适配器："
+  echo "1：QQBot（官方机器人）"
+  echo "2：ICQQ（普通机器人）"
+  echo "3：NTQQ"
+  read -p "选择一个选项: " adapter_choice
 
   case $adapter_choice in
-    1) configure_qqbot ;;
-    2) configure_icqq ;;
-    3) configure_ntqq ;;
-    *) echo '无效选项'; main_menu ;;
+  1) Configure_QQBot ;;
+  2) Configure_ICQQ ;;
+  3) Configure_NTQQ ;;
+  *) echo "无效选项"
+    Main_Menu
+    ;;
   esac
 }
 
-configure_qqbot() {
-  if ! git clone --depth 1 https://gitee.com/TimeRainStarSky/Yunzai-QQBot-Plugin plugins/QQBot-Plugin; then
-    echo -e "$Y- Gitee下载失败，尝试切换到GitHub$O"
-    if ! git clone --depth 1 https://github.com/TimeRainStarSky/Yunzai-QQBot-Plugin plugins/QQBot-Plugin; then
-      echo -e "$R! 你这破网是怎么回事！$O"
-      exit 1
-    fi
-  fi
-
-  node app.js &
+Configure_QQBot() {
+  git clone --depth 1 https://gitee.com/TimeRainStarSky/Yunzai-QQBot-Plugin plugins/QQBot-Plugin || abort_update "下载失败"
+  node app &
   sleep 5
-  if [ -f "$DIR/config/QQBot.yaml" ]; then
-    echo -e "$Y- 监听文件位置：Yunzai/config/QQBot.yaml$O"
-  else
-    echo -e "$R! Yunzai/config/QQBot.yaml 文件加载失败$O"
-    exit 1
-  fi
-
+  [ -f "$DIR/config/QQBot.yaml" ] || abort "配置文件加载失败：$DIR/config/QQBot.yaml"
   kill %1
 
-  # 获取设备公网 IP
-  echo '请选择获取公网IP方式（使用↑↓控制）'
-  select ip_choice in 自动获取 手动输入; do
-    if [ "$ip_choice" == "自动获取" ]; then
-      public_ip=$(curl -s ifconfig.me)
-    else
-      read -p '请输入你的公网IP: ' public_ip
-    fi
-    break
-  done
+  read -p "输入你的官方机器人QQ: " bot_qq
+  read -p "输入你的官方机器人ID: " bot_id
+  read -p "输入你的官方机器人Token: " bot_token
+  read -p "输入你的官方机器人AppSecret: " bot_secret
 
-  # 查看并修改 Yunzai/config/config/bot.yaml 文件
-  echo -e "$Y- 正在修改 Yunzai/config/config/bot.yaml 文件$O"
-  if [ -f "$DIR/config/config/bot.yaml" ]; then
-    sed -i "s|url:.*|url: http://$public_ip:2536|g" "$DIR/config/config/bot.yaml"
-
-    echo '是否开启 / 转 #（默认开启）'
-    select slash_to_hash in 开启 关闭; do
-      if [ "$slash_to_hash" == "开启" ]; then
-        sed -i "s|/→#:.*|/→#: true|g" "$DIR/config/config/bot.yaml"
-      else
-        sed -i "s|/→#:.*|/→#: false|g" "$DIR/config/config/bot.yaml"
-      fi
-      break
-    done
-  else
-    echo -e "$R! Yunzai/config/config/bot.yaml 文件不存在$O"
-    exit 1
-  fi
-
-  read -p '输入你的官方机器人QQ: ' bot_qq
-  read -p '输入你的官方机器人ID: ' bot_id
-  read -p '输入你的官方机器人Token: ' bot_token
-  read -p '输入你的官方机器人AppSecret: ' bot_secret
-
-  echo '是否有群权限（使用↑↓控制）'
+  echo "是否有群权限（使用↑↓控制）"
   select bot_group in 是 不是; do
-    if [ "$bot_group" == "是" ]; then
-      bot_group=1
-    else
-      bot_group=0
-    fi
+    bot_group=$( [ "$bot_group" == "是" ] && echo 1 || echo 0 )
     break
   done
 
-  echo '是否公域（使用↑↓控制）'
+  echo "是否公域（使用↑↓控制）"
   select bot_public in 是 不是; do
-    if [ "$bot_public" == "是" ]; then
-      bot_public=0
-    else
-      bot_public=1
-    fi
+    bot_public=$( [ "$bot_public" == "是" ] && echo 0 || echo 1 )
     break
   done
 
-  cat > "$DIR/config/QQBot.yaml" <<EOF
+  echo "是否开启/转#（使用↑↓控制）"
+  select bot_convert in 开启 关闭; do
+    bot_convert=$( [ "$bot_convert" == "开启" ] && echo true || echo false )
+    break
+  done
+
+  echo "请选择公网IP获取方式："
+  echo "1：手动输入"
+  echo "2：自动获取"
+  read -p "选择一个选项: " ip_choice
+
+  case $ip_choice in
+  1)
+    read -p "请输入你的公网IP: " public_ip
+    ;;
+  2)
+    public_ip=$(curl -s ifconfig.me)
+    echo "自动获取的公网IP为：$public_ip"
+    ;;
+  *)
+    echo "无效选项"
+    Main_Menu
+    ;;
+  esac
+
+  sed -i "s#url:.*#url: http://$public_ip:2536#" "$DIR/config/config/bot.yaml"
+  sed -i "s#/→#:#/→#: $bot_convert#" "$DIR/config/config/bot.yaml"
+
+  cat >"$DIR/config/QQBot.yaml" <<EOF
 tips:
   - 欢迎使用 TRSS-Yunzai QQBot Plugin ! 作者：时雨🌌星空
   - 参考：https://github.com/TimeRainStarSky/Yunzai-QQBot-Plugin
@@ -212,27 +180,14 @@ token:
   - $bot_qq:$bot_id:$bot_token:$bot_secret:$bot_group:$bot_public
 EOF
 
-  node app.js &
+  node app &
 }
 
-configure_icqq() {
-  if ! git clone --depth 1 https://gitee.com/TimeRainStarSky/Yunzai-ICQQ-Plugin plugins/ICQQ-Plugin; then
-    echo -e "$Y- Gitee下载失败，尝试切换到GitHub$O"
-    if ! git clone --depth 1 https://github.com/TimeRainStarSky/Yunzai-ICQQ-Plugin plugins/ICQQ-Plugin; then
-      echo -e "$R! 你这破网是怎么回事！$O"
-      exit 1
-    fi
-  fi
-
-  node app.js &
+Configure_ICQQ() {
+  git clone --depth 1 https://gitee.com/TimeRainStarSky/Yunzai-ICQQ-Plugin plugins/ICQQ-Plugin || abort_update "下载失败"
+  node app &
   sleep 5
-  if [ -f "$DIR/config/ICQQ.yaml" ]; then
-    echo -e "$Y- 监听文件位置：Yunzai/config/ICQQ.yaml$O"
-  else
-    echo -e "$R! Yunzai/config/ICQQ.yaml 文件加载失败$O"
-    exit 1
-  fi
-
+  [ -f "$DIR/config/ICQQ.yaml" ] || abort "配置文件加载失败：$DIR/config/ICQQ.yaml"
   kill %1
 
   echo -e "$Y- 正在检查ICQQ签名$O"
@@ -243,7 +198,7 @@ configure_icqq() {
 
   for url in "${sign_urls[@]}"; do
     start_time=$(date +%s%N)
-    curl -o /dev/null -s $url
+    curl -o /dev/null -s "$url"
     end_time=$(date +%s%N)
     latency=$(( (end_time - start_time) / 1000000 ))
 
@@ -255,7 +210,7 @@ configure_icqq() {
 
   echo -e "$Y- 已选签名${selected_url}，延迟${min_latency}ms，正在配置$O"
 
-  cat > "$DIR/config/ICQQ.yaml" <<EOF
+  cat >"$DIR/config/ICQQ.yaml" <<EOF
 tips:
   - 欢迎使用 TRSS-Yunzai ICQQ Plugin ! 作者：时雨🌌星空
   - 参考：https://github.com/TimeRainStarSky/Yunzai-ICQQ-Plugin
@@ -269,63 +224,65 @@ bot:
 token: []
 EOF
 
-  read -p '请输入你机器人的QQ: ' bot_qq
-  read -p '请输入你机器人的QQ密码: ' bot_password
+  read -p "请输入你机器人的QQ: " bot_qq
+  read -p "请输入你机器人的QQ密码: " bot_password
 
-  cat >> "$DIR/config/ICQQ.yaml" <<EOF
+  cat >>"$DIR/config/ICQQ.yaml" <<EOF
   - $bot_qq:$bot_password:2
 EOF
 
-  node app.js &
+  node app &
 }
 
-configure_ntqq() {
-  if ! git clone --depth 1 https://gitee.com/TimeRainStarSky/Yunzai-Lagrange-Plugin plugins/Lagrange-Plugin; then
-    echo -e "$Y- Gitee下载失败，尝试切换到GitHub$O"
-    if ! git clone --depth 1 https://github.com/TimeRainStarSky/Yunzai-Lagrange-Plugin plugins/Lagrange-Plugin; then
-      echo -e "$R! 你这破网是怎么回事！$O"
-      exit 1
-    fi
-  fi
-
-  node app.js &
+Configure_NTQQ() {
+  git clone --depth 1 https://gitee.com/TimeRainStarSky/Yunzai-Lagrange-Plugin plugins/Lagrange-Plugin || abort_update "下载失败"
+  node app &
   sleep 5
-  if [ -f "$DIR/config/Lagrange.yaml" ]; then
-    echo -e "$Y- 监听文件位置：Yunzai/config/Lagrange.yaml$O"
-  else
-    echo -e "$R! Yunzai/config/Lagrange.yaml 文件加载失败$O"
-    exit 1
-  fi
-
+  [ -f "$DIR/config/NTQQ.yaml" ] || abort "配置文件加载失败：$DIR/config/NTQQ.yaml"
   kill %1
 
-  echo -e "$Y- 启动测试成功，正在为你配置签名$O"
+  echo -e "$Y- 正在检查NTQQ签名$O"
+  sign_urls=('https://hlhs-nb.cn/signed/?key=114514' 'http://1.QSign.icu?key=XxxX' 'http://2.QSign.icu?key=XxxX' 'http://3.QSign.icu?key=XxxX' 'http://4.QSign.icu?key=XxxX' 'http://5.QSign.icu?key=XxxX')
 
-  cat > "$DIR/config/Lagrange.yaml" <<EOF
+  min_latency=9999
+  selected_url=''
+
+  for url in "${sign_urls[@]}"; do
+    start_time=$(date +%s%N)
+    curl -o /dev/null -s "$url"
+    end_time=$(date +%s%N)
+    latency=$(( (end_time - start_time) / 1000000 ))
+
+    if [ $latency -lt $min_latency ]; then
+      min_latency=$latency
+      selected_url=$url
+    fi
+  done
+
+  echo -e "$Y- 已选签名${selected_url}，延迟${min_latency}ms，正在配置$O"
+
+  cat >"$DIR/config/NTQQ.yaml" <<EOF
 tips:
-  - 欢迎使用 TRSS-Yunzai Lagrange Plugin ! 作者：时雨🌌星空
-  - 参考：https://github.com/TimeRainStarSky/Yunzai-Lagrange-Plugin
+  - 欢迎使用 TRSS-Yunzai NTQQ Plugin ! 作者：时雨🌌星空
+  - 参考：https://github.com/TimeRainStarSky/Yunzai-NTQQ-Plugin
 permission: master
 markdown:
   mode: false
   button: false
   callback: true
 bot:
-  signApiAddr: https://sign.libfekit.so/api/sign
+  sign_api_addr: $selected_url
 token: []
 EOF
 
-  read -p '请输入你机器人的QQ账号: ' bot_qq
-  read -p '请输入你机器人的QQ密码: ' bot_password
+  read -p "请输入你机器人的QQ: " bot_qq
+  read -p "请输入你机器人的QQ密码: " bot_password
 
-  cat >> "$DIR/config/Lagrange.yaml" <<EOF
-  - $bot_qq:$bot_password
+  cat >>"$DIR/config/NTQQ.yaml" <<EOF
+  - $bot_qq:$bot_password:2
 EOF
 
-  node app.js &
+  node app &
 }
 
-install_dependencies
-download
-install_plugins
-start_test
+echo -e "$G- 安装完成，输入 rabbit 以启动Yunzai$O"
